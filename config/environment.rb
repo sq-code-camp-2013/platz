7e031ae7c6a07037fa16d2e45234325db4ef771d
Erica Kwan <erica+code-camp@squareup.com>
2013-01-10 15:06:21 -0800

    Disable default XML param parser, symbol and yaml parsing in the parser

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,10 @@
+# Disable the default XML params parser
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
+# Disable symbol and yaml parsing in the XML parser
+ActiveSupport::XmlMini::PARSING.delete("symbol")
+ActiveSupport::XmlMini::PARSING.delete("yaml")
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
