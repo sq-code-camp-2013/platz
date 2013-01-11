d0f2357e177b3032122d1841c3e400023f3fcdbc
Erica Kwan <erica+code-camp@squareup.com>
2013-01-10 16:41:58 -0800

    Load the rails app first

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
 # Disable the default XML params parser
 ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
 
 # Disable symbol and yaml parsing in the XML parser
 ActiveSupport::XmlMini::PARSING.delete("symbol")
 ActiveSupport::XmlMini::PARSING.delete("yaml")
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Platz::Application.initialize!
